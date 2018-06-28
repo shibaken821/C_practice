4e39412a9aff63bd36691729cd48a2493bc92747
urakamikit <urakami.naoya149@mail.kyutech.jp>
2018-06-28 17:17:44 +0900

    Please comment each sentence.

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUMBER 40
+
+/*int function_t(int value) {
+	int a;
+
+	return a;
+}*/
+
 int main(void){
-
+	
+	//int a = function_t(3);
+	
 	int i;
-	int v[40];
+	int v[NUMBERE];
 
-	for (i = 0; i < 40; i++){
-		if (i == 0 || i == 1)
+	for (i = 0; i < NUMBERE; i++){
+		if (i == 0 || i == 1) {
 			v[i] = i;
-		else
+		} else {
 			v[i] = v[i - 1] + v[i - 2];
+		}
 		printf("%d\n",v[i]);
 	}
 
